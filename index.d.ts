c2322413fe2137c382c4486a884205063525775e
IIIMADDINIII <martin@elend.name>
2023-10-31 15:44:37 +0100

    First Version

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare module "consts:production" {
+  let production: boolean;
+  export default production;
+}
+declare module "consts:development" {
+  let development: boolean;
+  export default development;
+}
+declare module "consts:testing" {
+  let testing: boolean;
+  export default testing;
+}
